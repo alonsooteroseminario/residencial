cfacb91e5d00fa63760592709fae3ec23e171a3d
Luis Otero <alonsooteroseminario@gmail.com>
2021-09-16 19:28:55 -0500

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { transporterGmail } = require('./controllers/email');
 
 const app = express()
-const port = process.env.PORT.toString() || parseInt(process.argv[2]) || 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
